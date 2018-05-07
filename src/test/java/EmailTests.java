aaff1937e5a73b74d09869c9f558357e97ee77d8
Chris Robinson <cdrobinson.cdr@gmail.com>
2018-05-07 17:40:45 -0400

    Theoretically done?

--- src/test/java/EmailTests.java
+++ src/test/java/EmailTests.java
@@ -5,7 +5,6 @@ public class EmailTests {
 
     @Test
     public void testBuildConfirmationEmail(){
-        EmailManager mailer = new EmailManager();
         String bodyText = EmailManager.buildConfirmationEmail("ConfirmationCode");
         Assert.assertEquals("Hello Cardinal! \nPlease PM your confirmation message back to the Charlie Cardinal bot." +
                 "\nHere is your confirmation message: ConfirmationCodeConfirmationCode", bodyText);
@@ -13,8 +12,6 @@ public class EmailTests {
 
     @Test
     public void testBuildConfirmationEmailWithGenerator(){
-        EmailManager mailer = new EmailManager();
-        CodeGenerator codeGen = new CodeGenerator();
         String code = CodeGenerator.generateEmailCode();
         String bodyText = EmailManager.buildConfirmationEmail(code);
         Assert.assertEquals("Hello Cardinal! \nPlease PM your confirmation message back to the Charlie Cardinal bot." +
