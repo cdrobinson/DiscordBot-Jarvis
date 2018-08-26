6b697de0182a07fd7bc8f559b65fa947d6f86976
Chris <chrisdrobinsonrules@gmail.com>
2018-08-26 18:30:05 -0400

    Updated the !help message.

--- src/main/java/HelpMessageBuilder.java
+++ src/main/java/HelpMessageBuilder.java
@@ -4,6 +4,8 @@ class HelpMessageBuilder {
                 "!ping: Pong! [current ping to bot in ms] \r" +
                 "!bing: Bong! \r" +
                 "!help: Shows you this message \r" +
+                "================BSU Students Only================ \r" +
+                "!chirpchirp: Run this command after registering as a BSU student on the Cardinal Esports Discord server\r" +
                 "================These commands must be used in #sr-tracking================\r" +
                 "[####]: To set your SR, type only your SR into the #sr-tracking channel, ie. 2500\r" +
                 "!session start: Begins an SR tracking session. Will record your SR when the command is ran \r" +
