b48a28fab662b8a064a7c3f3ec3b979f553556c7
Chris <chrisdrobinsonrules@gmail.com>
2018-11-23 17:15:41 -0500

    PreChanging FeatureRequester over to Google Sheet storage

--- src/main/java/FeatureRequester.java
+++ src/main/java/FeatureRequester.java
@@ -3,12 +3,13 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class FeatureRequester {
 
     private String frChannelId;
-    private String pinnedMessageId;
+    private String latestPinnedMessageId;
     private String featureList;
 
     FeatureRequester() {
+
         this.frChannelId = null;
-        this.pinnedMessageId = null;
+        this.latestPinnedMessageId = null;
         this.featureList = null;
         loadFromFile();
     }
@@ -19,7 +20,7 @@ public class FeatureRequester {
         if (fileContents != null) {
             String[] featureList = fileContents.split("\n");
             this.frChannelId = featureList[0];
-            this.pinnedMessageId = featureList[1];
+            this.latestPinnedMessageId = featureList[1];
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(featureList[2]);
             for (int i = 3; i < featureList.length; i++) {
@@ -34,18 +35,18 @@ public class FeatureRequester {
         FileManager fileManager = new FileManager();
         String fileName = new ConfigManager().getProperty("guildName") + "_featureList.txt";
         String fileContents = this.frChannelId + "\n" +
-                this.pinnedMessageId + "\n" +
+                this.latestPinnedMessageId + "\n" +
                 this.featureList;
         fileManager.writeToTextFile(fileContents, fileName);
     }
 
     void repostFeatureList(MessageReceivedEvent event) {
-        event.getGuild().getTextChannelById(frChannelId).getMessageById(pinnedMessageId).queue((pinnedMessage) -> {
+        event.getGuild().getTextChannelById(frChannelId).getMessageById(latestPinnedMessageId).queue((pinnedMessage) -> {
             pinnedMessage.unpin().queue();
             pinnedMessage.delete().queue();
         });
         event.getGuild().getTextChannelById(this.frChannelId).sendMessage(this.featureList).queue((newMessage) -> {
-            this.pinnedMessageId = newMessage.getId();
+            this.latestPinnedMessageId = newMessage.getId();
             newMessage.pin().queue();
             saveFile();
         });
@@ -53,10 +54,10 @@ public class FeatureRequester {
 
     void addRequest(String request, MessageReceivedEvent event) {
         StringBuilder stringBuilder = new StringBuilder();
-        if (this.pinnedMessageId == null) {
+        if (this.latestPinnedMessageId == null) {
             event.getChannel().sendMessage("A feature list hasn't been setup yet. Please run !rfSetup first").queue();
         } else {
-            event.getGuild().getTextChannelById(frChannelId).getMessageById(pinnedMessageId).queue((pinnedMessage) -> {
+            event.getGuild().getTextChannelById(frChannelId).getMessageById(latestPinnedMessageId).queue((pinnedMessage) -> {
                 stringBuilder.append(pinnedMessage.getContentRaw());
                 stringBuilder.delete(stringBuilder.length() - 3, stringBuilder.length());
                 stringBuilder.append("⭕");
@@ -80,7 +81,7 @@ public class FeatureRequester {
         }
         StringBuilder updatedMessage = new StringBuilder();
         updatedMessage.append("List of requested features:\n⭕=Received | ✔=Added | ❌=Denied\n");
-        event.getChannel().getMessageById(pinnedMessageId).queue((pinnedMessage) -> {
+        event.getChannel().getMessageById(latestPinnedMessageId).queue((pinnedMessage) -> {
             String cleanRequests = pinnedMessage.getContentRaw().split("```")[1];
             String[] requestList = cleanRequests.split("\n");
             if (requestNumber > requestList.length || requestNumber < 1) {
@@ -110,7 +111,7 @@ public class FeatureRequester {
         }
         StringBuilder updatedMessage = new StringBuilder();
         updatedMessage.append("List of requested features:\n⭕=Received | ✔=Added | ❌=Denied\n");
-        event.getChannel().getMessageById(pinnedMessageId).queue((pinnedMessage) -> {
+        event.getChannel().getMessageById(latestPinnedMessageId).queue((pinnedMessage) -> {
             String cleanRequests = pinnedMessage.getContentRaw().split("```")[1];
             String[] requestList = cleanRequests.split("\n");
             if (requestNumber > requestList.length || requestNumber < 1) {
@@ -133,7 +134,7 @@ public class FeatureRequester {
     public void setUp(MessageReceivedEvent event) {
         String setupMessage = "List of requested features:\n⭕=Received | ✔=Added | ❌=Denied\n```\n```";
         event.getChannel().sendMessage(setupMessage).queue((featureListMessage) -> {
-            this.pinnedMessageId = featureListMessage.getId();
+            this.latestPinnedMessageId = featureListMessage.getId();
             featureListMessage.pin().queue();
             this.frChannelId = event.getChannel().getId();
             this.featureList = setupMessage;
